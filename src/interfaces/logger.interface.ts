6ae164f6d62dabdc0b67e69af219ce8adc56dacc
CVS CHARAN <charan.cvs@gmail.com>
2025-11-23 20:39:11 +0530

    fix: suppress ESLint warnings for logger interface
    - Added eslint-disable comments for any type in LoggerInterface
    - This is standard practice for generic logger interfaces
    - Lint now passes with 0 errors, 0 warnings

--- src/interfaces/logger.interface.ts
+++ src/interfaces/logger.interface.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface LoggerInterface {
   info(message: string, ...meta: any[]): void;
   error(message: string, ...meta: any[]): void;
   warn(message: string, ...meta: any[]): void;
   debug(message: string, ...meta: any[]): void;
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
